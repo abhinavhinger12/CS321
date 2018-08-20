61e6ae9b0ef1bd0f3c6d21aa0b18365b5b0eb424
Ansh Sood <32872608+ansh-sood@users.noreply.github.com>
2018-08-21 03:08:43 +0530

    commenting

--- Assignment 2/24 Hr Clock.asm
+++ Assignment 2/24 Hr Clock.asm
@@ -16,7 +16,7 @@ UPDAD: EQU 0440H
 MVI A,00H
 MVI B,00H
 
-LXI H,8840H
+LXI H,8840H	;[H] <- 88H and [L] <- 40H
 MVI M,0CH
 
 LXI H,8841H
@@ -29,8 +29,8 @@ LXI H,8843H
 MVI M,0CH
 
 LXI H,8840H
-CALL OUTPUT
-CALL RDKBD
+CALL OUTPUT	;displays CLOC on the address 7segment display
+CALL RDKBD	;waits for key board input from the user
 CALL CLEAR
 
 
@@ -41,15 +41,15 @@ MOV H,D
 MOV L,E
 
 FIRST:
-	MOV A,H
-	CPI 24H
+	MOV A,H		;move content of H into A
+	CPI 24H		;compare content of accumulator with 24H
 	JC SECOND
 START:
 	MVI H,00H
 	JC TWENTY_FOUR
 SECOND:
 	MOV A,L
-	CPI 60H
+	CPI 60H		;compare content of accumlator with 60H 
 	JC TWENTY_FOUR
 THIRD:
 	MVI L,00H 	
@@ -59,25 +59,25 @@ TWENTY_FOUR:
 	;LXI H,0000H
 
 HR_MIN:
-	SHLD CURAD
+	SHLD CURAD	; store H-L pair at CURAD memory location
 	MVI A,00H
 NXT_SEC:
-	STA CURDT
-	CALL UPDAD
-	CALL UPDDT
-	CALL DELAY
+	STA CURDT	; store content of A at CURDT memory location
+	CALL UPDAD	;displays content at CURAD memory location on address 7field
+	CALL UPDDT	;displays content at CURDT memory location on data field
+	CALL DELAY	;calls the delay 
 	LDA CURDT
-	ADI 01H
-	DAA
+	ADI 01H		;add 01H to the content of A
+	DAA		;decimal adjustment of A (0A is converted to 10)
 	CPI 60H
-	JNZ NXT_SEC
-	LHLD CURAD
+	JNZ NXT_SEC	;jump if not zero to NXT_SEC
+	LHLD CURAD	;load H-L pair with value at CURAD memory location
 	MOV A,L
 	ADI 01H
 	DAA
 	MOV L,A
 	CPI 60H
-	JNZ HR_MIN
+	JNZ HR_MIN	;jump if not zero to HR_MIN
 	MVI L,00H
 	MOV A,H
 	ADI 01H
@@ -92,11 +92,11 @@ DELAY:
 OUTLOOP:
 	LXI D,0A700H
 INLOOP:
-	DCX D
+	DCX D		; decrease D-E register pair by 1
 	MOV A,D
-	ORA E
+	ORA E		;logical or of contents of A with contents of E
 	JNZ INLOOP
-	DCR C
+	DCR C		;decrease C by 1
 	JNZ OUTLOOP
 	RET
 
