8a7d01f4387855ea2f9a60eb1be8c4e93ee38fab
Abhinav Hinger <abhinavhinger12@gmail.com>
2018-08-27 23:26:47 -0700

    commenting

--- Assignment 3/led_light.asm
+++ Assignment 3/led_light.asm
@@ -9,34 +9,42 @@ CLEAR: EQU 02BEH
 
 org 9000h 
 
-MVI A,8BH
+MVI A,8BH		;setting 8255 to mode0 and port A o/p and port B,C i/p
 MVI B,01H
 OUT 43H
 START:
-	IN 41H
-	CPI 01H
+	IN 41H		; [A] <- IN from 8255
+	CPI 40H		; compare immediate with 40H (continuing condition)
 	JZ CONT
+	ANI 04H		; logical and 04H with content of A 
+	CPI 04H		; 04H is ending condition 
+	JZ END	
 	JMP START
 CONT:
 	MOV A,B
 	OUT 40H
 	MOV A,B
-	RLC
+	RLC		; rotste(in cyclic fashion) content of A to left by one 
 	MOV B,A
 	IN 41H
-	ANI 03H
-	CPI 03H
-	JZ START
-	CALL DELAY
+	ANI 60H
+	CPI 60H		;60H is pause condition
+	JZ START	
+	IN 41H
+	ANI 04H
+	CPI 04H
+	JZ END
+	CALL DELAY	; delay function for 1sec delay
 	JMP CONT
+END: 	RST 5		;end of program
 DELAY:
 	MVI C,03H
 OUTLOOP:
 	LXI D,0AF00H
 INLOOP:
-	DCX D
+	DCX D		; decrease the D-E pair by one
 	MOV A,D
-	ORA E
+	ORA E		; logical or with content of A
 	JNZ INLOOP
 	DCR C
 	JNZ OUTLOOP
